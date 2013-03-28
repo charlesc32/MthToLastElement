5a02440fa3b2429f3b1a71434fad6b5f63104e37
ccymbor <ccymbor@gmail.com>
2013-03-28 15:34:31 -0700

    init

---
+++ FindMthCharTests/ProgramTest.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace FindMthCharTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ProgramTest and is intended
+    ///to contain all ProgramTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProgramTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for FindMthChar
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MthToLastElement.exe")]
+        public void FindMthCharTest()
+        {
+            IEnumerable<string> array = new List<string>() { "a", "b", "c", "d" }; // TODO: Initialize to an appropriate value
+            int index = 4; // TODO: Initialize to an appropriate value
+            string expected = "a"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program_Accessor.FindMthChar(array, index);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("MthToLastElement.exe")]
+        public void FindMthCharTest2()
+        {
+            IEnumerable<string> array = new List<string>() { "e", "f", "g", "h" }; // TODO: Initialize to an appropriate value
+            int index = 2; // TODO: Initialize to an appropriate value
+            string expected = "g"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program_Accessor.FindMthChar(array, index);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("MthToLastElement.exe")]
+        public void FindMthCharTest3()
+        {
+            IEnumerable<string> array = new List<string>() { "e", "f", "g", "h" }; // TODO: Initialize to an appropriate value
+            int index = 2; // TODO: Initialize to an appropriate value
+            string expected = "g"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program_Accessor.FindMthChar(array, index);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for FindMthChar
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MthToLastElement.exe")]
+        public void FindMthCharTest1()
+        {
+            IEnumerable<string> array = new List<string>() { "e", "f", "g", "h" }; // TODO: Initialize to an appropriate value
+            int index = 5; // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program_Accessor.FindMthChar(array, index);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MthToLastElement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                var input = line.Split(' ');
+                //Make sure we are at least getting 2 arguments on the line
+                if (input.Length < 2) continue;
+
+                //Split the line into input array and index (return if index is not a valid int)
+                var array = input.Take(input.Length - 1);
+                int index = 0;
+                if (!int.TryParse(input[input.Length - 1], out index)) return;
+
+                var output = FindMthChar(array, index);
+                if (output.Length > 0) Console.WriteLine(output);
+            }
+    }
+
+    private static string FindMthChar(IEnumerable<string> array, int index)
+    {
+        if (index > array.Count() || index < 0) return string.Empty;
+
+        return array.ElementAt(array.Count() - index);
+    }
+}
